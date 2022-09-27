fd9d5868726ff0859b618aa4d9c4e171011ca271
MaiconGiehl <maicongiehl@gmail.com>
2022-09-26 22:15:03 -0300

    chorus: add users resource

---
+++ migrations/V1_create_table_user.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS users
+(
+	id INT NOT NULL PRIMARY KEY,
+	name VARCHAR (255) NOT NULL,
+	active BOOLEAN NOT NULL,
+	Created_In TIMESTAMP,
+	Updated_In TIMESTAMP
+);
+
+
+INSERT INTO users
+(
+	id, name, active, created_in, updated_in
+)
+VALUES
+(
+	1, 'Leandro', true, NOW()::timestamp, NOW()::timestamp
+)
